ad6e23e0ac400d001447bc95dd777ae7de2bc616
Subhadeep Chell <sherlockx90@gmail.com>
2022-08-29 10:07:30 +0530

    first commit

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype
+{
+ public static void main(String[] args)
+ {
+  	byte b = 23;
+  	short s = 2345; 
+	int i=12345;
+	long L= 136456315468L;
+	float f=11111.22f;
+	char c ='B';
+	double d= 3455.5;
+	String st="JAVA";
+	boolean bo=true;
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(i);
+	System.out.println(L);
+	System.out.println(f);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(st);
+	System.out.println(bo);
+ }
+}
